09e48241fe321d62bf9fd9b19edd17889e787a7b
Mark Tranter <mark.a.tranter@gmail.com>
2018-03-06 11:43:40 +1100

    Remove my old ass

--- site/src/profile/index.js
+++ site/src/profile/index.js
@@ -6,7 +6,6 @@ module.exports = {
   heading: "About Me",
   personalDetails: {
     name: "Mark Tranter",
-    dob: '1979-12-01',
     address: 'Newcastle, NSW, Australia',
     phone: '0499 769 278',
     email: 'mark.a.tranter@gmail.com',
