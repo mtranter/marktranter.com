8db5dd59dfc243f9f56212985c5ed8869b52a54a
Mark Tranter <mark.a.tranter@gmail.com>
2018-07-20 09:32:08 +1000

    Update other contact details

--- site/src/profile/index.js
+++ site/src/profile/index.js
@@ -6,7 +6,7 @@ module.exports = {
   heading: "About Me",
   personalDetails: {
     name: "Mark Tranter",
-    address: 'Newcastle, NSW, Australia',
+    address: 'Sydney, NSW, Australia',
     phone: '0499 769 278',
     email: 'mark.a.tranter@gmail.com',
     nationality: 'Australian'
